ce95ea48a43e55d093f3121d56b8910fa6e73fed
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-06-25 14:58:22 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/chuck_norris_jokes_curso/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chuck_norris_jokes_curso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Number Trivia',
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
